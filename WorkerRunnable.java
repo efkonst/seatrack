72fd6480d0a8ee43dee7e0642d00f7406f1ec4b0
Efthimios Konstantinidis <efkonst@gmail.com>
2016-03-22 00:42:02 +0200

    first server

---
+++ WorkerRunnable.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.StringTokenizer;
+/**
+
+ */
+public class WorkerRunnable implements Runnable{
+
+    protected Socket clientSocket = null;
+    protected String serverText   = null;
+
+    public WorkerRunnable(Socket clientSocket, String serverText) {
+        this.clientSocket = clientSocket;
+        this.serverText   = serverText;
+    }
+
+    public void run() {
+        try {
+            InputStream input  = clientSocket.getInputStream();
+            OutputStream output = clientSocket.getOutputStream();
+            long time = System.currentTimeMillis();
+
+    System.out.println("GOT DATA");
+    BufferedReader br =new BufferedReader(new InputStreamReader(input));
+    String got_data_from_gps= br.readLine();
+    String[] got_1 = got_data_from_gps.split(","); 
+
+    System.out.println(got_1[1]+":"+got_1[3]);
+    output.write(("*HQ,"+got_1[1]+",D1,"+got_1[3]+",30,1#\r\n").getBytes());
+   while(true){
+      got_data_from_gps= br.readLine();
+      System.out.println(got_data_from_gps);
+    output.write(("\r\n").getBytes());
+     }
+//        output.close();
+//        input.close();
+
+//            input.close();
+//            System.out.println("Request processed: " + time);
+        } catch (IOException e) {
+            //report exception somewhere.
+            e.printStackTrace();
+        }
+    }
+}

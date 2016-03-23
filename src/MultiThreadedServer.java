c313cf12e95f7b9dd36d8cad43e7a40c06cf079a
Efthimios Konstantinidis <efkonst@gmail.com>
2016-03-24 00:25:53 +0200

    commit 2

--- src/MultiThreadedServer.java
+++ src/MultiThreadedServer.java
@@ -2,63 +2,58 @@ import java.net.ServerSocket;
 import java.net.Socket;
 import java.io.IOException;
 
-public class MultiThreadedServer implements Runnable{
+public class MultiThreadedServer implements Runnable {
 
-    protected int          serverPort   = 8080;
-    protected ServerSocket serverSocket = null;
-    protected boolean      isStopped    = false;
-    protected Thread       runningThread= null;
+	protected int serverPort = 8080;
+	protected ServerSocket serverSocket = null;
+	protected boolean isStopped = false;
+	protected Thread runningThread = null;
 
-    public MultiThreadedServer(int port){
-        this.serverPort = port;
-    }
+	public MultiThreadedServer(int port) {
+		this.serverPort = port;
+	}
 
-    public void run(){
-        synchronized(this){
-            this.runningThread = Thread.currentThread();
-        }
-        openServerSocket();
-	System.out.println("Server Started");
-        while(! isStopped()){
-            Socket clientSocket = null;
-            try {
-                clientSocket = this.serverSocket.accept();
-            } catch (IOException e) {
-                if(isStopped()) {
-                    System.out.println("Server Stopped.") ;
-                    return;
-                }
-                throw new RuntimeException(
-                    "Error accepting client connection", e);
-            }
-            new Thread(
-                new WorkerRunnable(
-                    clientSocket, "Multithreaded Server")
-            ).start();
-        }
-        System.out.println("Server Stopped.") ;
-    }
+	public void run() {
+		synchronized (this) {
+			this.runningThread = Thread.currentThread();
+		}
+		openServerSocket();
+		System.out.println("Server Started");
+		while (!isStopped()) {
+			Socket clientSocket = null;
+			try {
+				clientSocket = this.serverSocket.accept();
+			} catch (IOException e) {
+				if (isStopped()) {
+					System.out.println("Server Stopped.");
+					return;
+				}
+				throw new RuntimeException("Error accepting client connection", e);
+			}
+			new Thread(new WorkerRunnable(clientSocket, "Multithreaded Server")).start();
+		}
+		System.out.println("Server Stopped.");
+	}
 
+	private synchronized boolean isStopped() {
+		return this.isStopped;
+	}
 
-    private synchronized boolean isStopped() {
-        return this.isStopped;
-    }
+	public synchronized void stop() {
+		this.isStopped = true;
+		try {
+			this.serverSocket.close();
+		} catch (IOException e) {
+			throw new RuntimeException("Error closing server", e);
+		}
+	}
 
-    public synchronized void stop(){
-        this.isStopped = true;
-        try {
-            this.serverSocket.close();
-        } catch (IOException e) {
-            throw new RuntimeException("Error closing server", e);
-        }
-    }
-
-    private void openServerSocket() {
-        try {
-            this.serverSocket = new ServerSocket(this.serverPort);
-        } catch (IOException e) {
-            throw new RuntimeException("Cannot open port 8080", e);
-        }
-    }
+	private void openServerSocket() {
+		try {
+			this.serverSocket = new ServerSocket(this.serverPort);
+		} catch (IOException e) {
+			throw new RuntimeException("Cannot open port 8080", e);
+		}
+	}
 
 }

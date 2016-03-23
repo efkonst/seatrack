afec1b5b87047c69e112347fcd9240ee95356e64
Efthimios Konstantinidis <efkonst@gmail.com>
2016-03-24 00:03:03 +0200

    add send to http Server

--- src/WorkerRunnable.java
+++ src/WorkerRunnable.java
@@ -4,45 +4,58 @@ import java.io.OutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.util.StringTokenizer;
+import java.net.*;
+
 /**
 
  */
-public class WorkerRunnable implements Runnable{
+public class WorkerRunnable implements Runnable {
 
-    protected Socket clientSocket = null;
-    protected String serverText   = null;
+	protected Socket clientSocket = null;
+	protected String serverText = null;
 
-    public WorkerRunnable(Socket clientSocket, String serverText) {
-        this.clientSocket = clientSocket;
-        this.serverText   = serverText;
-    }
+	public WorkerRunnable(Socket clientSocket, String serverText) {
+		this.clientSocket = clientSocket;
+		this.serverText = serverText;
+	}
 
-    public void run() {
-        try {
-            InputStream input  = clientSocket.getInputStream();
-            OutputStream output = clientSocket.getOutputStream();
-            long time = System.currentTimeMillis();
+	public void run() {
+		try {
+			InputStream input = clientSocket.getInputStream();
+			OutputStream output = clientSocket.getOutputStream();
+			long time = System.currentTimeMillis();
 
-//    System.out.println("GOT DATA");
-    BufferedReader br =new BufferedReader(new InputStreamReader(input));
-    String got_data_from_gps= br.readLine();
-    String[] got_1 = got_data_from_gps.split(","); 
+			// System.out.println("GOT DATA");
+			BufferedReader br = new BufferedReader(new InputStreamReader(input));
+			String got_data_from_gps = br.readLine();
+			String[] got_1 = got_data_from_gps.split(",");
 
-//    System.out.println(got_1[1]+":"+got_1[3]);
-    output.write(("*HQ,"+got_1[1]+",D1,"+got_1[3]+",30,1#\r\n").getBytes());
-   while(true){
-      got_data_from_gps= br.readLine();
-//      System.out.println(got_data_from_gps);
-    output.write(("\r\n").getBytes());
-     }
-//        output.close();
-//        input.close();
+			// System.out.println(got_1[1]+":"+got_1[3]);
+			output.write(("*HQ," + got_1[1] + ",D1," + got_1[3] + ",30,1#\r\n").getBytes());
+			while (true) {
+				got_data_from_gps = br.readLine();
+				if (!got_data_from_gps.contains("XT")) {
+					HttpClient htclient = new HttpClient();
 
-//            input.close();
-//            System.out.println("Request processed: " + time);
-        } catch (IOException e) {
-            //report exception somewhere.
-//            e.printStackTrace();
-        }
-    }
+					try {
+						htclient.sendGet();
+					} catch (Exception e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+
+				}
+				System.out.println(got_data_from_gps);
+				output.write(("\r\n").getBytes());
+			}
+			// output.close();
+			// input.close();
+
+			// input.close();
+			// System.out.println("Request processed: " + time);
+		} catch (IOException e) {
+			// report exception somewhere.
+			// e.printStackTrace();
+		}
+	}
 }

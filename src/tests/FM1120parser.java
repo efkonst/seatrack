790d60fd24c82c976ffa97134c2318c2da472691
Efthimios Konstantinidis <efkonst@gmail.com>
2016-04-05 17:15:25 +0300

    crc add

--- src/tests/FM1120parser.java
+++ src/tests/FM1120parser.java
@@ -19,5 +19,27 @@ public class FM1120parser {
 		
 		
 	}
+	
+	public static int getCrc16(Byte[] buffer) {
+	    return getCrc16(buffer, 0, buffer.length, 0xA001, 0);
+	    }
+
+	public synchronized static int getCrc16(Byte[] buffer, int offset, int bufLen, int polynom, int preset) {
+	    preset &= 0xFFFF;
+	    polynom &= 0xFFFF;
+	    int crc = preset;
+	    for (int i = 0; i < bufLen; i++) {
+	        int data = buffer[i + offset] & 0xFF;
+	        crc ^= data;
+	        for (int j = 0; j < 8; j++) {
+	            if ((crc & 0x0001) != 0) {
+	                crc = (crc >> 1) ^ polynom;
+	            } else {
+	                crc = crc >> 1;
+	            }
+	        }
+	    }
+	    return crc & 0xFFFF;
+	}
 
 }

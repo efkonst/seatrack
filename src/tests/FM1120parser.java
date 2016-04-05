8edee147ee8cbcf311c7c177a9c7e01a159ebe24
Efthimios Konstantinidis <efkonst@gmail.com>
2016-04-05 17:11:14 +0300

    add FM1120

---
+++ src/tests/FM1120parser.java
@@ -0,0 +1,23 @@
+package tests;
+
+public class FM1120parser {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		FM1120 fm1120=new FM1120("080400000113fc208dff000f14f650209cca80006f00d60400040004030101150316030001460000015d0000000113fc17610b000f14ffe0209cc580006e00c00500010004030101150316010001460000015e0000000113fc284945000f150f00209cd200009501080400000004030101150016030001460000015d0000000113fc267c5b000f150a50209cccc0009300680400000004030101150016030001460000015b0004");
+		
+/*		for (int i=0;i<fm1120.data.length;i++)
+			System.out.println(String.format("%02x", fm1120.data[i]));
+*/
+		
+/*		int i = ((0xFF & fm1120.data[0]) << 24) | ((0xFF & fm1120.data[1]) << 16) |
+	            ((0xFF & fm1120.data[2]) << 8) | (0xFF & fm1120.data[3]);
+*/
+		
+//		System.out.println(fm1120.speed);
+		
+		
+	}
+
+}
---
+++ src/tests/Record.java
@@ -0,0 +1,38 @@
+package tests;
+
+public class Record {
+	long timestamp;
+	int priority;
+	float longitude;
+	float lattitude;
+	int altitude;
+	int heading;
+	int sats;
+	int speed;
+
+	Record(long t, int pr, int lon, int lat, int alt, int head, int sat, int spd) {
+		this.timestamp = t;
+		this.priority = pr;
+		this.longitude = lon;
+		this.lattitude = lat;
+		this.altitude = alt;
+		this.heading = head;
+		this.sats = sat;
+		this.speed = spd;
+
+	}
+
+	public String toString(){
+		
+		return "time   : "+this.timestamp+"\r\n"+
+  			   "prio   : "+this.priority+"\r\n"+
+  			   "long   : "+this.longitude/10000000+"\r\n"+
+  			   "latt   : "+this.lattitude/10000000+"\r\n"+
+  			   "alt    : "+this.altitude+"\r\n"+
+  			   "heading: "+this.heading+"\r\n"+
+  			   "sats   : "+this.sats+"\r\n"+
+  			   "speed  : "+this.speed+"\r\n";
+	
+	}
+
+}
